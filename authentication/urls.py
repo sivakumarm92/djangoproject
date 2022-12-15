dd87e3db51da8a2226c12ddfed7a80bfd9af4e94
sivakumarm92 <100434482+sivakumarm92@users.noreply.github.com>
2022-12-15 15:33:51 +0530

    Add files via upload

---
+++ authentication/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('signup', views.signup, name="Signup"),
+    path('signin', views.signin, name="SignIn"),
+    path('signout', views.signout, name="SignOut"),
+]
